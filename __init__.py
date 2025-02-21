70584a9226f7cd7d0b6b62d4ad519d7f8eb86b6a
catia91100 <catia.catarino@yahoo.fr>
2025-02-21 17:01:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,20 +11,43 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-key = Fernet.generate_key()
-f = Fernet(key)
+get_fernet(user_key):
+key_bytes = user_key.encode()
+return Fernet(key_bytes)
+except Exception as e:
+return None
 
-@app.route('/encrypt/<string:valeur>')
-def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-
-@app.route('/decrypt/<string:token>')
+@app.route('/encrypt', methods ['GET'])
+def encryptage():
+    valeur = request.args.get('valeur')
+    user_key = request.args.get('key')
+  if not valeur or not user_key:
+    return "Erreur: veuillez donner à la fois la 'valeur' et la 'Key'"
+  
+  f = get_fernet(user_key)
+if not f:
+  return "Erreur: clé invalide.Assurrez vous d'utiliser une clé Fernet valide (32 url-safe base64-encoded bytes)."
+  
+@app.route('/decrypt', methods=['GET'])
 def decryptage(token):
     token_bytes = token.encode()  # Conversion str -> bytes
-    valeur = f.decrypt(token_bytes)  # décrypt la valeur
-    return f"Valeur décryptée : {valeur.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+    token = request.args.get('token')
+    user_key = request.args.get('key')
+  if not token or not user_key:
+    return "Erreur: veuillez donner à la fois la 'token' et la 'Key'"
+    
+     f = get_fernet(user_key)
+if not f:
+  return "Erreur: clé invalide.Assurrez vous d'utiliser une clé Fernet valide (32 url-safe base64-encoded bytes)."
+
+try:
+  token_bytes = token.encode()
+  valeur = f.decrypt(token_bytes)
+  return f"Valeur décryptée : {valeur.decode()}
+  except InvalidToken:
+  return "Erreur: échec du décryptage.clé ou token invalide."
+    except Exception as e:
+    return return f"Erreur lors du décryptage: {str(e)}"
+                                                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
